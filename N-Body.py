00d0890c9a615235afdcc94ebf345454bb586010
ajhook <141203324+ajhook@users.noreply.github.com>
2024-11-21 22:44:19 +0000

    Energy change

--- N-Body.py
+++ N-Body.py
@@ -16,12 +16,25 @@ vx2i = 0
 vy1i = -15 *km
 vy2i = 15 *km
 
-r = np.sqrt((x2i - x1i) ** 2 + (y2i - y1i) ** 2)
 
 m2 = 1.989e30
 m1 = m2
 
 #Define functions
+def calculateEnergy(state,m1,m2):
+    x1, x2, y1, y2, vx1, vx2, vy1, vy2 = state
+
+    k1 = 0.5 * m1 * (vx1**2 + vy1**2)
+    k2 = 0.5 * m2 * (vx2**2 + vy2**2)
+    K = k1+k2
+
+    r = np.sqrt((x2i - x1i) ** 2 + (y2i - y1i) ** 2)
+
+    U = ((-G) * m1 * m2) / r
+
+
+    return K+U
+
 def testCase(state, t, G, m1, m2):
     x1, x2, y1, y2, vx1, vx2, vy1, vy2 = state
 
@@ -44,7 +57,11 @@ def testCase(state, t, G, m1, m2):
 initial = [x1i, x2i, y1i, y2i, vx1i, vx2i, vy1i, vy2i]
 
 t = np.linspace(0, 5 * 365.25 * 24 * 60 * 60, 1000) #Create time array
+T = t/365*24*60*60
 sol = odeint(testCase, initial, t, args=(G, m1, m2)) #Solve ODE's
+
+E = np.array([calculateEnergy(state,m1,m2) for state in sol])
+dE = (E - E[0])/E[0]
 
 #Extract values on Astronomical Units
 x1 = sol[:,0]/AU
@@ -58,6 +75,9 @@ plt.axis('equal')
 plt.plot(x1,y1)
 plt.plot(x2,y2)
 plt.figure(2,figsize=(12,9))
-plt.plot(t,x1)
-plt.plot(t,x2)
+plt.plot(T,x1)
+plt.plot(T,x2)
+plt.figure(3)
+plt.plot(T,dE)
 plt.show()
+
