2a2b8ccd9b42c73c4c7ee8c1bb0dd20830566624
ajhook <141203324+ajhook@users.noreply.github.com>
2024-12-04 22:11:59 +0000

    Energy change

--- BurrausProblem.py
+++ BurrausProblem.py
@@ -3,24 +3,16 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-#Constants
-G = 6.67e-11
-AU = 1.496e11
-km = 1000
-pc = 3.0857e16
-M0 = 1.989e30
-
-#Masses
-m1 = 3 * M0
-m2 = 4 * M0
-m3 = 5 * M0
+#Constants (dimensionless units)
+G = 1
+m1,m2,m3 = 3,4, 5
 
 
 #Initial Conditions
 #Postions
-x1i, y1i = 1*pc, 3*pc
-x2i, y2i = -2*pc, -1*pc
-x3i, y3i = 1*pc,-1*pc
+x1i, y1i = 1, 3
+x2i, y2i = -2, -1
+x3i, y3i = 1,-1
 
 
 #Velocities
@@ -30,9 +22,27 @@ vx3i, vy3i = 0, 0
 
 initialConditions = [x1i, y1i, x2i, y2i, x3i, y3i, vx1i, vy1i, vx2i, vy2i, vx3i, vy3i]
 
-#Define Function
+#Define Functions
+def calculateEnergy(state, m1, m2, m3):
+    x1, y1, x2, y2, x3, y3, vx1, vy1, vx2, vy2, vx3, vy3 = state
+
+
+
+    k1 = 0.5 * m1 * (vx1 ** 2 + vy1 ** 2)
+    k2 = 0.5 * m2 * (vx2 ** 2 + vy2 ** 2)
+    k3 = 0.5 * m3 * (vx3 ** 2 + vy3 ** 2)
+    K = k1 + k2 + k3
+
+    r_12 = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    r_13 = np.sqrt((x3 - x1) ** 2 + (y3 - y1) ** 2)
+    r_23 = np.sqrt((x3 - x2) ** 2 + (y3 - y2) ** 2)
+
+    U = -G*(((m1 * m2) / r_12) + ((m2*m3)/ r_23) + ((m1*m3)/r_13))
+
+    return K + U
 def threeBodySystem(state, G, t, m1, m2, m3):
     x1, y1, x2, y2, x3, y3, vx1, vy1, vx2, vy2, vx3, vy3 = state
+
 
     #Distances
     r_12 = np.sqrt((x2-x1)**2 + (y2-y1)**2)
@@ -56,21 +66,36 @@ def threeBodySystem(state, G, t, m1, m2, m3):
 
     return [dx1, dy1, dx2, dy2, dx3, dy3, dvx1, dvy1, dvx2, dvy2, dvx3, dvy3]
 
-time = np.linspace(0,2*365.25*24*60*60, 100000)
+time = np.linspace(3, 6, 1000000)
 
-solution = odeint(threeBodySystem, initialConditions, time, args = (G, m1, m2, m3))
+sol = odeint(threeBodySystem, initialConditions, time, args = (G, m1, m2, m3))
 
-x1 = solution[:, 0] / AU
-y1 = solution[:, 1] / AU
-x2 = solution[:, 2] / AU
-y2 = solution[:, 3] / AU
-x3 = solution[:, 4] / AU
-y3 = solution[:, 5] / AU
+E = np.array([calculateEnergy(state,m1,m2,m3) for state in sol])
+dE = abs((E - E[0])/E[0])
 
-plt.figure(3)
-plt.plot(x1, y1, label="M1")
-plt.plot(x2, y2, label="M2")
-plt.plot(x3, y3, label="M3")
+x1 = sol[:, 0]
+y1 = sol[:, 1]
+x2 = sol[:, 2]
+y2 = sol[:, 3]
+x3 = sol[:, 4]
+y3 = sol[:, 5]
+
+plt.figure(figsize=(10, 10))
+plt.plot(x1, y1, ':', label=f'm1={m1}', alpha=0.7)  # dotted line for m1
+plt.plot(x2, y2, '--', label=f'm2={m2}', alpha=0.7)  # dashed line for m2
+plt.plot(x3, y3, '-', label=f'm3={m3}', alpha=0.7)  # solid line for m3
+
+# Plot initial positions
+plt.figure(1)
+plt.plot([x1[0]], [y1[0]], 'ko')
+plt.plot([x2[0]], [y2[0]], 'ko')
+plt.plot([x3[0]], [y3[0]], 'ko')
 plt.legend()
+
+plt.figure(figsize=(8, 6))
+plt.plot(time, dE, label="Relative Energy Deviation")
+plt.xlabel("Time")
+plt.ylabel(r"$\Delta E / E_0$")
+plt.title("Energy Conservation")
 plt.grid()
 plt.show()
---
+++ StarSystem.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+G = 6.67e-11
+AU = 1.496e11
+km = 1000
+M0 = 1.988e30
+ME = 5.97226e24
+
+Ma = 0.146 * M0
+Mb = 5.6 * ME
+Mc = 1.272 * ME
+
+#Orbital Periods
+Tb = 24.7 * 24 * 3600
+Tc = 3.8 * 24 * 3600
+#Orbital Radius
+rb = 0.0946 * AU
+rc = 0.02734 * AU
+
+#Habitable Zone
+innerHZ = 0.06*AU
+outerHZ = 0.087*AU
+
+#Positions
+xAi, yAi = 0,0
+xBi, yBi = rb, 0
+xCi, yCi = -rc, 0
+xEi, yEi = 500000*AU, 0
+
+#Velocities
+vxAi, vyAi = 0, 0
+vxBi, vyBi = 0, 2* np.pi * (rb/Tb)
+vxCi, vyCi = 0, -2* np.pi * (rc/Tc)
+vxEi, vyEi = 0,0
+
+initialConditions = [xAi, yAi, xBi, yBi, xCi, yCi, xEi, yEi, vxAi, vyAi, vxBi, vyBi, vxCi, vyCi, vxEi, vyEi]
+
+def fourBodySystem(state, t, G, Ma, Mb, Mc, ME,):
+    xA, yA, xB, yB, xC, yC, xE, yE, vxA, vyA, vxB, vyB, vxC, vyC, vxE, vyE = state
+
+    # Distances
+    r_AB = np.sqrt((xB - xA) ** 2 + (yB - yA) ** 2)
+    r_AC = np.sqrt((xC - xA) ** 2 + (yC - yA) ** 2)
+    r_AE = np.sqrt((xE - xA) ** 2 + (yE - yA) ** 2)
+    r_BC = np.sqrt((xC - xB) ** 2 + (yC - yB) ** 2)
+    r_BE = np.sqrt((xE - xB) ** 2 + (yE - yB) ** 2)
+    r_CE = np.sqrt((xE - xC) ** 2 + (yE - yC) ** 2)
+
+    # Velocities
+    dxA, dyA = vxA, vyA
+    dxB, dyB = vxB, vyB
+    dxC, dyC = vxC, vyC
+    dxE, dyE = vxE, vyE
+
+    # Accelerations
+    dvxA = (G * (Mb / r_AB ** 3) * (xB - xA)) + (G * (Mc / r_AC ** 3) * (xC - xA)) + (G * (ME / r_AE ** 3) * (xE - xA))
+    dvyA = (G * (Mb / r_AB ** 3) * (yB - yA)) + (G * (Mc / r_AC ** 3) * (yC - yA)) + (G * (ME / r_AE ** 3) * (yE - yA))
+
+    dvxB = (G * (Ma / r_AB ** 3) * (xA - xB)) + (G * (Mc / r_BC ** 3) * (xC - xB)) + (G * (ME / r_BE ** 3) * (xE - xB))
+    dvyB = (G * (Ma / r_AB ** 3) * (yA - yB)) + (G * (Mc / r_BC ** 3) * (yC - yB)) + (G * (ME / r_BE ** 3) * (yE - yB))
+
+    dvxC = (G * (Ma / r_AC ** 3) * (xA - xC)) + (G * (Mb / r_BC ** 3) * (xB - xC)) + (G * (ME / r_CE ** 3) * (xE - xC))
+    dvyC = (G * (Ma / r_AC ** 3) * (yA - yC)) + (G * (Mb / r_BC ** 3) * (yB - yC)) + (G * (ME / r_CE ** 3) * (yE - yC))
+
+    dvxE = (G * (Ma / r_AE ** 3) * (xA - xE)) + (G * (Mb / r_BE ** 3) * (xB - xE)) + (G * (Mc / r_CE ** 3) * (xC - xE))
+    dvyE = (G * (Ma / r_AE ** 3) * (yA - yE)) + (G * (Mb / r_BE ** 3) * (yB - yE)) + (G * (Mc / r_CE ** 3) * (yC - yE))
+
+    return [dxA, dyA, dxB, dyB, dxC, dyC, dxE, dyE, dvxA, dvyA, dvxB, dvyB, dvxC, dvyC, dvxE, dvyE]
+
+t = np.linspace(0, 10*Tb, 1000000)
+sol = odeint(fourBodySystem, initialConditions, t, args=(G, Ma, Mb, Mc, ME))
+
+xA = sol[:, 0]/AU
+yA = sol[:, 1]/AU
+xB = sol[:, 2]/AU
+yB = sol[:, 3]/AU
+xC = sol[:, 4]/AU
+yC = sol[:, 5]/AU
+xE = sol[:, 6]/AU
+yE = sol[:, 7]/AU
+
+plt.figure(figsize=(10, 10))
+plt.plot(xA, yA, label=f'LHS-1140-A', color='r')
+plt.plot(xB, yB, label=f'LHS-1140-B', color='b')
+plt.plot(xC, yC, label=f'LHS-1140-C', color='g')
+plt.plot(xE, yE, label=f'Earth', color ='y')
+plt.xlim(-0.2,0.2)
+plt.ylim(-0.2,0.2)
+plt.xlabel("X(AU)")
+plt.ylabel("Y(AU)")
+plt.legend()
+plt.grid()
+plt.show()
